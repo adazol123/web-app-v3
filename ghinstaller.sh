83d82a1206a01e97883bf04fc926e05de39fd9c8
Danyel <dpadayao40@gmail.com>
2022-06-05 07:47:04 +0800

    added comment on github cli installe

--- ghinstaller.sh
+++ ghinstaller.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+#######################################################R###
+#							  #
+#	 Github CLI installer for Linux environment	  #
+#							  #
+######################################################R####
+
+#fetch the repository of the installer 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+#update linux package maanger
 sudo apt update
+
+#install the github cli environment
 sudo apt install gh
 
 echo "GITHUB CLI Installation Success!"
